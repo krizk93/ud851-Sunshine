74152f8e163b9cf815f18a7ec9a09fa4be658e9d
krizk93 <karim.rizk@live.com>
2017-11-25 01:50:26 +0100

    solution to exercise 2

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,25 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
-
+    // COMPLETED (1) Create a field to store the weather display TextView
+    TextView WeatherDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+        WeatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // COMPLETED (2) Use findViewById to get a reference to the weather display TextView
 
-        // TODO (3) Create an array of Strings that contain fake weather data
-
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // COMPLETED (3) Create an array of Strings that contain fake weather data
+            String[] weatherData = {"Monday: sunny", "Tuesday: rain", "Wednesday: fog", "Thursday: heavy snow", "Friday: wind", "Saturday: cloudy", "Sunday: hail"};
+        // COMPLETED (4) Append each String from the fake weather data array to the TextView
+        for (int i=0; i< weatherData.length; i++){
+            WeatherDisplay.append(weatherData[i] + "\n\n\n");
+        }
     }
 }
