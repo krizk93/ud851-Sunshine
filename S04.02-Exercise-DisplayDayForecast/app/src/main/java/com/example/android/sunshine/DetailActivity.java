954e058cbd3eb0e98884215f8cd17ece779804c1
krizk93 <karim.rizk@live.com>
2017-12-10 07:14:47 +0100

    Solution to Exercise-DisplayDayForecast

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,27 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
+    TextView detailTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        detailTextView = (TextView) findViewById(R.id.tv_display_weather);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        // COMPLETED (2) Display the weather forecast that was passed from MainActivity
+
+        Intent intent = getIntent();
+        if(intent.hasExtra(Intent.EXTRA_TEXT)){
+            String detailText = intent.getStringExtra(Intent.EXTRA_TEXT);
+            detailTextView.setText(detailText);
+        }
     }
 }
